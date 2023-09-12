7a0b6bc8ae7f5925aa85b8658c412c2ecbf7cd3f
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2023-09-12 09:30:17 -0400

    https protocal

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
+  # config.force_ssl = true
   # Enable server timing
   config.server_timing = true
 
